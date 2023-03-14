08449eb5388c2d316b183b8c6d8dee525bf18d70
AdriaL-FIB <113378642+AdriaL-FIB@users.noreply.github.com>
2023-03-14 13:49:24 +0100

    archivos proj tiled

---
+++ night-knight/levels/tmx/blue-tileset16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="blue-tileset16" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../../images/blue-tileset16.png" width="128" height="128"/>
+</tileset>
