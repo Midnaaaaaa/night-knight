537b5fd87067f2e62962cfaff8c7a06c3efa7def
Midnaaaaaa <ijuaagi@gmail.com>
2023-03-25 19:14:08 +0100

    No va una puta mierda 💩

--- night-knight/Player.cpp
+++ night-knight/Player.cpp
@@ -18,13 +18,13 @@
 
 enum CharacterAnims
 {
-	MOVE_LEFT, MOVE_RIGHT, STAND_LEFT, STAND_RIGHT, CROUCH_LEFT, CROUCH_RIGHT, JUMP_ASCENDING, JUMP_ASCENDING_RIGHT1, 
+	MOVE_LEFT, MOVE_RIGHT, STAND_LEFT, STAND_RIGHT, CROUCH_LEFT, CROUCH_RIGHT, JUMP_ASCENDING, JUMP_ASCENDING_RIGHT, JUMP_ASCENDING_LEFT, JUMP_ASCENDING_MIRROR, FALLING1, FALLING_MIRROR1, FALLING2, FALLING_MIRROR2, FALLING3, FALLING_MIRROR3, FALLING_LEFT1, FALLING_RIGHT1, FALLING_LEFT2, FALLING_RIGHT2, FALLING_LEFT3, FALLING_RIGHT3, FALL_ANIMATION, FALL_ANIMATION_RIGHT, FALL_ANIMATION_LEFT, FALL_ANIMATION_MIRROR
 };
 
 
 
 void Player::loadAnimations() {
-	sprite->setNumberAnimations(6);
+	sprite->setNumberAnimations(26);
 
 	sprite->setAnimationParams(STAND_LEFT, 4, true);
 	sprite->addKeyframe(STAND_LEFT, glm::vec2(0.0f, 0.0f));
@@ -82,6 +82,89 @@ void Player::loadAnimations() {
 	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(1 / 16.f * 6, 1 / 16.f * 1));
 
 
+	sprite->setAnimationParams(JUMP_ASCENDING, 10, false);
+	sprite->addKeyframe(JUMP_ASCENDING, glm::vec2(1 / 16.f * 7, 1 / 16.f * 1));
+	sprite->addKeyframe(JUMP_ASCENDING, glm::vec2(1 / 16.f * 8, 1 / 16.f * 1));
+
+	sprite->setAnimationParams(JUMP_ASCENDING_MIRROR, 10, true);
+	sprite->addKeyframe(JUMP_ASCENDING_MIRROR, glm::vec2(1 / 16.f * 7, 1 / 16.f * 1));
+	sprite->addKeyframe(JUMP_ASCENDING_MIRROR, glm::vec2(1 / 16.f * 8, 1 / 16.f * 1));
+
+
+
+	sprite->setAnimationParams(JUMP_ASCENDING_LEFT, 1, true);
+	sprite->addKeyframe(JUMP_ASCENDING_LEFT, glm::vec2(1 / 16.f * 9,  1 / 16.f * 1));
+	sprite->addKeyframe(JUMP_ASCENDING_LEFT, glm::vec2(1 / 16.f * 10, 1 / 16.f * 1));
+	sprite->addKeyframe(JUMP_ASCENDING_LEFT, glm::vec2(1 / 16.f * 11, 1 / 16.f * 1));
+
+	sprite->setAnimationParams(JUMP_ASCENDING_RIGHT, 1, false);
+	sprite->addKeyframe(JUMP_ASCENDING_RIGHT, glm::vec2(1 / 16.f * 9, 1 / 16.f * 1));
+	sprite->addKeyframe(JUMP_ASCENDING_RIGHT, glm::vec2(1 / 16.f * 10, 1 / 16.f * 1));
+	sprite->addKeyframe(JUMP_ASCENDING_RIGHT, glm::vec2(1 / 16.f * 11, 1 / 16.f * 1));
+
+
+
+
+	sprite->setAnimationParams(FALLING_MIRROR1, 1, true);
+	sprite->addKeyframe(FALLING_MIRROR1, glm::vec2(1 / 16.f * 9, 1 / 16.f * 1));
+
+	sprite->setAnimationParams(FALLING1, 1, false);
+	sprite->addKeyframe(FALLING1, glm::vec2(1 / 16.f * 9, 1 / 16.f * 1));
+
+
+	sprite->setAnimationParams(FALLING_MIRROR2, 1, true);
+	sprite->addKeyframe(FALLING_MIRROR2, glm::vec2(1 / 16.f * 10, 1 / 16.f * 1));
+
+	sprite->setAnimationParams(FALLING2, 1, false);
+	sprite->addKeyframe(FALLING2, glm::vec2(1 / 16.f * 10, 1 / 16.f * 1));
+
+
+
+	sprite->setAnimationParams(FALLING_MIRROR3, 1, true);
+	sprite->addKeyframe(FALLING_MIRROR3, glm::vec2(1 / 16.f * 11, 1 / 16.f * 1));
+
+	sprite->setAnimationParams(FALLING3, 1, false);
+	sprite->addKeyframe(FALLING3, glm::vec2(1 / 16.f * 11, 1 / 16.f * 1));
+
+
+	sprite->setAnimationParams(FALLING_LEFT1, 1, true);
+	sprite->addKeyframe(FALLING_LEFT1, glm::vec2(1 / 16.f * 12, 1 / 16.f * 1));
+
+	sprite->setAnimationParams(FALLING_RIGHT1, 1, false);
+	sprite->addKeyframe(FALLING_RIGHT1, glm::vec2(1 / 16.f * 12, 1 / 16.f * 1));
+
+
+	sprite->setAnimationParams(FALLING_LEFT2, 1, true);
+	sprite->addKeyframe(FALLING_LEFT2, glm::vec2(1 / 16.f * 13, 1 / 16.f * 1));
+
+	sprite->setAnimationParams(FALLING_RIGHT2, 1, false);
+	sprite->addKeyframe(FALLING_RIGHT2, glm::vec2(1 / 16.f * 13, 1 / 16.f * 1));
+
+
+	sprite->setAnimationParams(FALLING_LEFT3, 1, true);
+	sprite->addKeyframe(FALLING_LEFT3, glm::vec2(1 / 16.f * 14, 1 / 16.f * 1));
+
+	sprite->setAnimationParams(FALLING_RIGHT3, 1, false);
+	sprite->addKeyframe(FALLING_RIGHT3, glm::vec2(1 / 16.f * 14, 1 / 16.f * 1));
+
+
+	sprite->setAnimationParams(FALL_ANIMATION, 8, false, 2);
+	sprite->addKeyframe(FALL_ANIMATION, glm::vec2(1 / 16.f * 11, 1 / 16.f * 1));
+
+
+	sprite->setAnimationParams(FALL_ANIMATION_MIRROR, 8, true, 2);
+	sprite->addKeyframe(FALL_ANIMATION_MIRROR, glm::vec2(1 / 16.f * 11, 1 / 16.f * 1));
+
+
+	sprite->setAnimationParams(FALL_ANIMATION_RIGHT, 8, false, 2);
+	sprite->addKeyframe(FALL_ANIMATION_RIGHT, glm::vec2(1 / 16.f * 14, 1 / 16.f * 1));
+
+
+	sprite->setAnimationParams(FALL_ANIMATION_LEFT, 8, true, 1);
+	sprite->addKeyframe(FALL_ANIMATION_LEFT, glm::vec2(1 / 16.f * 14, 1 / 16.f * 1));
+
+
+
 	sprite->setAnimationParams(CROUCH_LEFT, 8, true);
 	sprite->addKeyframe(CROUCH_LEFT, glm::vec2(1 / 8.f * 4, 0.0f));
 
@@ -95,45 +178,6 @@ void Player::loadAnimations() {
 void Player::update(int deltaTime)
 {	
 	sprite->update(deltaTime);
-	if(Game::instance().getSpecialKey(GLUT_KEY_LEFT))
-	{
-		if (sprite->animation() != MOVE_LEFT) {
-			sprite->changeAnimation(MOVE_LEFT);
-			rightSight = false;
-		}
-		posCharacter.x -= moveSpeed;
-		if(map->collisionMoveLeft(posCharacter + colliderOffset, colliderSize, bJumping))
-		{
-			posCharacter.x += moveSpeed;
-			sprite->changeAnimation(STAND_LEFT);
-		}
-	}
-	else if(Game::instance().getSpecialKey(GLUT_KEY_RIGHT))
-	{
-		if (sprite->animation() != MOVE_RIGHT) {
-			sprite->changeAnimation(MOVE_RIGHT);
-			rightSight = true;
-		}
-		posCharacter.x += moveSpeed;
-		if(map->collisionMoveRight(posCharacter + colliderOffset, colliderSize, bJumping))
-		{
-			posCharacter.x -= moveSpeed;
-			sprite->changeAnimation(STAND_RIGHT);
-		}
-	}
-	else if (Game::instance().getSpecialKey(GLUT_KEY_DOWN)) {
-		if (rightSight && sprite->animation() != CROUCH_RIGHT) {
-			sprite->changeAnimation(CROUCH_RIGHT);
-		}
-		else if (!rightSight && sprite->animation() != CROUCH_LEFT) {
-			sprite->changeAnimation(CROUCH_LEFT);
-		}
-	}
-	else
-	{
-		if(rightSight && sprite->animation() != STAND_RIGHT) sprite->changeAnimation(STAND_RIGHT);
-		else if(!rightSight && sprite->animation() != STAND_LEFT) sprite->changeAnimation(STAND_LEFT);
-	}
 	
 	if(bJumping)
 	{
@@ -165,7 +209,66 @@ void Player::update(int deltaTime)
 			}
 		}
 		//Cambiar animaciones de salto
-
+		if (jumpAngle < 90) {
+			if (Game::instance().getSpecialKey(GLUT_KEY_RIGHT)) {
+				rightSight = true;
+				sprite->changeAnimation(JUMP_ASCENDING_RIGHT);
+			}
+			else if (Game::instance().getSpecialKey(GLUT_KEY_LEFT)) {
+				rightSight = false;
+				if (sprite->animation() != JUMP_ASCENDING_LEFT) {
+					sprite->changeAnimation(JUMP_ASCENDING_LEFT);
+				}
+			}
+			else {
+				if (rightSight && sprite->animation() != JUMP_ASCENDING) {
+					sprite->changeAnimation(JUMP_ASCENDING);
+				}
+				else if(!rightSight && sprite->animation() != JUMP_ASCENDING_MIRROR) sprite->changeAnimation(JUMP_ASCENDING_MIRROR);
+			}
+		}
+		else if (jumpAngle < 135) {
+			if (Game::instance().getSpecialKey(GLUT_KEY_RIGHT)) {
+				rightSight = true;
+				sprite->changeAnimation(FALLING_RIGHT1);
+			}
+			else if (Game::instance().getSpecialKey(GLUT_KEY_LEFT)) {
+				rightSight = false;
+				sprite->changeAnimation(FALLING_LEFT1);
+			}
+			else {
+				if (rightSight) sprite->changeAnimation(FALLING1);
+				else sprite->changeAnimation(FALLING_MIRROR1);
+			}
+		}
+		else if (jumpAngle > 135 && jumpAngle < 150) {
+			if (Game::instance().getSpecialKey(GLUT_KEY_RIGHT)) {
+				rightSight = true;
+				sprite->changeAnimation(FALLING_RIGHT2);
+			}
+			else if (Game::instance().getSpecialKey(GLUT_KEY_LEFT)) {
+				rightSight = false;
+				sprite->changeAnimation(FALLING_LEFT2);
+			}
+			else {
+				if (rightSight) sprite->changeAnimation(FALLING2);
+				else sprite->changeAnimation(FALLING_MIRROR2);
+			}
+		}
+		else if (jumpAngle > 150) {
+			if (Game::instance().getSpecialKey(GLUT_KEY_RIGHT)) {
+				rightSight = true;
+				sprite->changeAnimation(FALLING_RIGHT3);
+			}
+			else if (Game::instance().getSpecialKey(GLUT_KEY_LEFT)) {
+				rightSight = false;
+				sprite->changeAnimation(FALLING_LEFT3);
+			}
+			else {
+				if (rightSight) sprite->changeAnimation(FALLING3);
+				else sprite->changeAnimation(FALLING_MIRROR3);
+			}
+		}
 	}
 	else
 	{
@@ -178,6 +281,59 @@ void Player::update(int deltaTime)
 				bJumping = true;
 				jumpAngle = 0;
 				startY = posCharacter.y;
+			}
+			if (Game::instance().getSpecialKey(GLUT_KEY_LEFT))
+			{
+				if (sprite->animation() != MOVE_LEFT) {
+					sprite->changeAnimation(MOVE_LEFT);
+					rightSight = false;
+				}
+				posCharacter.x -= moveSpeed;
+				if (map->collisionMoveLeft(posCharacter + colliderOffset, colliderSize, bJumping))
+				{
+					posCharacter.x += moveSpeed;
+					sprite->changeAnimation(STAND_LEFT);
+				}
+			}
+			else if (Game::instance().getSpecialKey(GLUT_KEY_RIGHT))
+			{
+				if (sprite->animation() != MOVE_RIGHT) {
+					sprite->changeAnimation(MOVE_RIGHT);
+					rightSight = true;
+				}
+				posCharacter.x += moveSpeed;
+				if (map->collisionMoveRight(posCharacter + colliderOffset, colliderSize, bJumping))
+				{
+					posCharacter.x -= moveSpeed;
+					sprite->changeAnimation(STAND_RIGHT);
+				}
+			}
+			else if (Game::instance().getSpecialKey(GLUT_KEY_DOWN)) {
+				if (rightSight && sprite->animation() != CROUCH_RIGHT) {
+					sprite->changeAnimation(CROUCH_RIGHT);
+				}
+				else if (!rightSight && sprite->animation() != CROUCH_LEFT) {
+					sprite->changeAnimation(CROUCH_LEFT);
+				}
+			}
+			else
+			{
+				if (rightSight && sprite->animation() != STAND_RIGHT) sprite->changeAnimation(STAND_RIGHT);
+				else if (!rightSight && sprite->animation() != STAND_LEFT) sprite->changeAnimation(STAND_LEFT);
+			}
+		}
+		else {
+			if (Game::instance().getSpecialKey(GLUT_KEY_RIGHT)) {
+				rightSight = true;
+				sprite->changeAnimation(FALL_ANIMATION_RIGHT);
+			}
+			else if (Game::instance().getSpecialKey(GLUT_KEY_LEFT)) {
+				rightSight = false;
+				sprite->changeAnimation(FALL_ANIMATION_LEFT);
+			}
+			else {
+				if (rightSight) sprite->changeAnimation(FALL_ANIMATION);
+				else sprite->changeAnimation(FALL_ANIMATION_MIRROR);
 			}
 		}
 		
